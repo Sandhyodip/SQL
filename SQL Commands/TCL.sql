64e9b4720bbdf0c52244c64572aca3c4930d6cca
sandhyodip <sandhyodip@samarth.ac.in>
2024-07-24 00:00:53 +0530

    TCL Commands

--- SQL Commands/TCL.sql
+++ SQL Commands/TCL.sql
@@ -27,3 +27,24 @@ DELETE FROM `employee_table` WHERE `id`=4; -- NOT EXECUTED i.e. ROLLED BACK
 ROLLBACK TO Second_Save_Point;
 INSERT INTO `employee_table` (`id`,`name`,`email`) VALUES (7,'Sanchari','sghosh@gmail.com'); -- EXECUTED
 COMMIT;
+
+-- Example of normal Transaction
+START TRANSACTION;
+UPDATE `employee_table` SET balance = balance - 8000 WHERE id = '6' AND `name` LIKE '%Sandhyodip%';
+UPDATE `employee_table` SET balance = balance + 8000 WHERE id = '7' AND `name` LIKE '%Sanchari%';
+COMMIT;
+ROLLBACK;
+
+-- Example of Isolation (I of ACID property) in Transaction
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+START TRANSACTION;
+-- Transaction 1
+UPDATE `employee_table` SET balance = balance - 8000 WHERE id = '7' AND `name` LIKE '%Sanchari%';
+UPDATE `employee_table` SET balance = balance + 8000 WHERE id = '6' AND `name` LIKE '%Sandhyodip%';
+COMMIT;
+-- Transaction 2
+UPDATE `employee_table` SET balance = balance - 3000 WHERE id = '1' AND `name` LIKE '%John%';
+UPDATE `employee_table` SET balance = balance + 3000 WHERE id = '2' AND `name` LIKE '%Smith%';
+COMMIT;
+ROLLBACK;
+
