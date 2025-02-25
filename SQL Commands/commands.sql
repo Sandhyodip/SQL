af3dab82a0986d8293ecaf5da8d526f18de0701f
sandhyodip <sandhyodip@samarth.ac.in>
2025-02-26 04:28:35 +0530

    Learning procedure in Mysql

---
+++ SQL Commands/commands.sql
@@ -0,0 +1,8 @@
+-- Create a database
+CREATE DATABASE `samridula`
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- The utf8mb4 character set, which can store a wide range of Unicode characters including emojis and other symbols.
+-- The utf8mb4_unicode_ci collation, which sorts and compares strings in a case-insensitive manner according to the Unicode standard.
+
