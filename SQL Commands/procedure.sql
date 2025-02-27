150f198abc79bc4147bb297c6c253a88801ee589
sandhyodip <sandhyodip@samarth.ac.in>
2025-02-27 10:50:00 +0530

    Learning procedure in Mysql

--- SQL Commands/procedure.sql
+++ SQL Commands/procedure.sql
@@ -91,3 +91,84 @@ DELIMITER ;
 CALL addNumbers(27,30);
 
 SHOW PROCEDURE STATUS WHERE Db = 'ignou_se';
+
+DELIMITER //
+CREATE PROCEDURE addEvenPhoneNo(
+IN phoneNo INT
+)
+BEGIN
+DECLARE start_value INT DEFAULT 12;
+WHILE start_value<=phoneNo DO
+IF MOD(start_value,2)=0 THEN
+INSERT INTO `sandhyodip` (`mobile_phone_no`) VALUES (start_value);
+END IF;
+SET start_value=start_value+1;
+END WHILE;
+END //
+DELIMITER ;
+
+CALL addEvenPhoneNo(20);
+
+DELIMITER //
+CREATE PROCEDURE addOddPhoneNo(
+IN phNo INT
+)
+BEGIN
+DECLARE counter INT DEFAULT 13;
+WHILE counter <=phNo DO
+IF MOD (counter,2) !=0 THEN
+INSERT INTO `sandhyo`(`mobile_phone_no`) VALUES (counter);
+END IF;
+SET counter=counter+1;
+END WHILE;
+END //
+DELIMITER ;
+CALL addOddPhoneNo(21);
+
+-- Write a procedure to find even & odd numbers between two given numbers (input) 
+DELIMITER //
+CREATE PROCEDURE getEvenOddNumber(
+IN start_value INT(10),
+IN end_value INT (10)
+)
+BEGIN
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_table (message VARCHAR(255));
+WHILE start_value < end_value DO
+IF MOD(start_value,2)=0 THEN
+INSERT INTO temp_table VALUES (CONCAT(start_value,': Even Number!'));
+ELSE 
+INSERT INTO temp_table VALUES (CONCAT(start_value,': Odd Number!'));
+END IF;
+SET start_value=start_value+1;
+END WHILE;
+SELECT * FROM temp_table;
+DROP TEMPORARY TABLE temp_table;
+END //
+DELIMITER ;
+
+CALL getEvenOddNumber(1,20);
+
+DELIMITER //
+CREATE PROCEDURE getEmployee(
+IN input_id INT
+)
+BEGIN
+SELECT `first_name`, `last_name` FROM `ems_profile` WHERE `id`=input_id;
+END //
+DELIMITER ;
+
+CALL getEmployee(1);
+
+
+DELIMITER //
+CREATE PROCEDURE getEmployeeName(
+IN input_id INT,
+OUT result VARCHAR(50)
+)
+BEGIN
+SELECT CONCAT(`first_name`,' ',`last_name`) INTO result FROM `ems_profile` WHERE `id`=input_id;
+END //
+DELIMITER ;
+
+CALL getEmployeeName(1,@employeeName);
+SELECT @employeeName AS Employee_Name;
